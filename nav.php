7e3f4bc0606d5554f8dfc5eac6a9a1cf8c7957c7
williamwarnockBSU <williamwarnock@u.boisestate.edu>
2018-09-23 11:40:24 -0600

    One more time

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <div class="nav">
     <ul id="navList">
-        <li><a<?php if ($thisPage=="index") echo " id=\"currentpage\""; ?> href="index.php">Main Page</a></li>
-        <li><a<?php if ($thisPage=="plannedTrips") echo " id=\"currentpage\""; ?> href="plannedTrips.php">Planned Trips</a></li>
-        <li><a<?php if ($thisPage=="messages") echo " id=\"currentpage\""; ?> href="messages.php">Messages</a></li>
-        <li><a<?php if ($thisPage=="tripsBoard") echo " id=\"currentpage\""; ?> href="tripsBoard.php">Trips Board</a></li>
+        <li><a<?php if ($thisPage=="index") echo " class=\"currentpage\""; ?> href="index.php">Main Page</a></li>
+        <li><a<?php if ($thisPage=="plannedTrips") echo " class=\"currentpage\""; ?> href="plannedTrips.php">Planned Trips</a></li>
+        <li><a<?php if ($thisPage=="messages") echo " class=\"currentpage\""; ?> href="messages.php">Messages</a></li>
+        <li><a<?php if ($thisPage=="tripsBoard") echo " class=\"currentpage\""; ?> href="tripsBoard.php">Trips Board</a></li>
     </ul>
 </div>
