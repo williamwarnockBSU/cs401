572e0a718e30a87c5885003e591fb533d1359737
williamwarnock <williamwarnock@u.boisestate.edu>
2018-09-22 21:57:28 -0600

    Last try for the night

--- messages.php
+++ messages.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-	<link href="/css/website.css" type="text/css" rel="stylesheet" />
+	<link href="website.css" type="text/css" rel="stylesheet" />
 		<title>Messages</title>
 	</head>
 	<body>
