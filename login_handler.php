aef16eef84aea4a6f9c92253bc01418ce8aa4950
williamwarnock <cwarnock1@gmail.com>
2018-11-05 10:57:05 -0700

    Can login with database

--- login_handler.php
+++ login_handler.php
@@ -8,13 +8,15 @@
   $password = $_POST['password'];
 
   $userExists = $dao->getUser($login, $password);
-  
-  if ($login == 'conrad' && $password == 'abc123') {
-  //if ($userExists != 0) {
-    $_SESSION['logged_in'] = true;
-    header('Location: mainPage.php');
-    exit;
+
+  foreach ($userExists as $user) {
+    if ($user['count(*)'] != 0) {
+      $_SESSION['logged_in'] = true;
+      header('Location: mainPage.php');
+      exit;
+    }
   }
+  
   $_SESSION['logged_in'] = false;
   $_SESSION['message'] = "Username or password invalid";
   header('Location: index.php');
